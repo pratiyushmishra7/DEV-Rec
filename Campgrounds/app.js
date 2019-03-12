4b659761b88e7888922d4abaa832595759200d2a
Pratiyush Mishra <pratiyushmishra7@gmail.com>
2019-03-12 14:44:39 +0530

    Update app.js

--- Campgrounds/app.js
+++ Campgrounds/app.js
@@ -87,5 +87,5 @@ app.use(commentRoutes);
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+   console.log("The Campgrounds Server Has Started!");
 });
